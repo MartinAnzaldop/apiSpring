4da654aeaec094a8448a262428e37be8fd57312e
MartinAnzaldop <126999311+MartinAnzaldop@users.noreply.github.com>
2023-04-04 23:41:38 -0600

    Update BebidaControlador.java

--- src/main/java/back/tienda/controlador/BebidaControlador.java
+++ src/main/java/back/tienda/controlador/BebidaControlador.java
@@ -14,6 +14,7 @@ import back.tienda.repositorio.BebidaRepositorio;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "*")
 public class BebidaControlador {
       
 	@Autowired
